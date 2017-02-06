f45cb0d08818772513501783e09d5a53cf038fb8
yllieth <sylvnimes@hotmail.com>
2017-02-05 21:13:17 -0500

    Save previous settings in localstorage

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 angular
   .module('ExtensionApp', [])
-  .constant('APIKEY', 'b2a905f079253f43cbda5191c62cb3d2')
   .constant('ENDPOINT', 'http://apilayer.net/api/live')
   .constant('CURRENCIES', ['EUR', 'USD', 'CAD', 'GBP'])
   .constant('CACHELIMIT', 604800) /* 7 * 24 * 60 * 60 = 7 jours */
-  .controller('PopupController', function($q, $http, APIKEY, CURRENCIES, CACHELIMIT, ENDPOINT) {
+  .controller('PopupController', function($q, $http, CURRENCIES, CACHELIMIT, ENDPOINT) {
     var self = this;
 
     // --------------------------------------------------------------------------
@@ -24,7 +23,9 @@ angular
 
     function fetchData() {
       if (localStorage) {
-        var mostRecent = Math.max(...Object.keys(localStorage));
+        var timestampPattern = new RegExp(/[0-9]{10}/);
+        var storedTimestamps = Object.keys(localStorage).filter(function(setting) { return timestampPattern.test(setting) === true });
+        var mostRecent = Math.max(...storedTimestamps);
         var time = now(true);  // timestamp in seconds
 
         if (time - mostRecent < CACHELIMIT) {
@@ -48,7 +49,7 @@ angular
         var config = {
           headers: {},
           params: {
-            access_key: APIKEY,
+            access_key: self.apiKey,
             currencies: CURRENCIES.join(','),
             source: 'USD',
             format: 1
@@ -86,6 +87,9 @@ angular
 
           self.result = amount * self.rate.value + charges;
         });
+
+      savePreviousBankCharge(self.bank.charges);
+      savePreviousBankChargeType(self.bank.selectedType);
     }
 
     // The free plan of currencylayer.com only allow to requests USD-based rates (USD -> EUR, USD -> CAD, ...)
@@ -99,6 +103,22 @@ angular
       } else {
         return rates['USD' + to] / rates['USD' + from];
       }
+    }
+
+    // Save settings
+    function saveApiKey(value)                      { localStorage.setItem('apiKey', value); }
+    function savePreviousOriginCurrency(value)      { localStorage.setItem('previousOriginCurrency', value); }
+    function savePreviousDestinationCurrency(value) { localStorage.setItem('previousDestinationCurrency',value); }
+    function savePreviousBankCharge(value)          { localStorage.setItem('previousBankCharge', value); }
+    function savePreviousBankChargeType(value)      { localStorage.setItem('previousBankChargeType', value); }
+
+    function initialize() {
+      self.apiKey            = localStorage.getItem('apiKey')                      || 'b2a905f079253f43cbda5191c62cb3d2';
+      self.selector.from     = localStorage.getItem('previousOriginCurrency')      || '---';
+      self.selector.to       = localStorage.getItem('previousDestinationCurrency') || '---';
+      self.bank.charges      = parseInt(localStorage.getItem('previousBankCharge'))|| 0;
+      self.bank.selectedType = localStorage.getItem('previousBankChargeType')      || 'percentage';
+      self.onBaseChange();
     }
 
     // --------------------------------------------------------------------------
@@ -144,6 +164,7 @@ angular
       });
 
       this.readyToConvert = isSettedUp(this.selector);
+      savePreviousOriginCurrency(this.selector.from);
 
       if (this.readyToConvert === true) {
         this.toConvert = 1;
@@ -154,6 +175,7 @@ angular
     this.onDestChange = function() {
       this.readyToConvert = isSettedUp(this.selector);
       this.bank.types[1].display = this.selector.to;
+      savePreviousDestinationCurrency(this.selector.to);
 
       if (this.readyToConvert === true) {
         this.toConvert = 1;
@@ -191,7 +213,11 @@ angular
         });
         this.selector.to = old.from;
         this.bank.types[1].display = this.selector.to;
+        savePreviousOriginCurrency(this.selector.from);
+        savePreviousDestinationCurrency(this.selector.to);
         convert(this.toConvert, this.selector.from, this.selector.to);
       }
     };
+
+    initialize();
   });
